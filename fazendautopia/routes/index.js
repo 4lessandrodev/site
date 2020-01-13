a29f9dc0b4c73cf09bece4d2f3a3f2f387f242d2
Alessandro Menezes <alessandroadm@live.com>
2020-01-13 00:26:55 -0300

    iniciando as alterações na pagina home

--- fazendautopia/routes/index.js
+++ fazendautopia/routes/index.js
@@ -1,12 +1,22 @@
 var express = require('express');
+var produtos = require('./../inc/produtos');
+var cestas = require('./../inc/cestas');
 var router = express.Router();
 
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Express',
-    home: true
+  produtos.getProdutosAtivos().then(produtos => {
+    cestas.getCestasAtivas().then(cestas => {
+      res.render('index', {
+        title: 'Express',
+        home: true,
+        produtos,
+        cestas
+      });
+    }).catch(err => {
+      res.send(err);
+    });
   });
 });
 
